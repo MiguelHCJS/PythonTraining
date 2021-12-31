cc096c326f9730945e5900fe85dafa91c2670d1d
Miguel Tenório <69335370+MiguelHCJS@users.noreply.github.com>
2021-12-30 23:46:52 -0300

    Atualização e Loop for

---
+++ src/loops/loopFor.py
@@ -0,0 +1,9 @@
+''' ESTRUTURA DE REPETIÇÃO UM POUCO DIFERENTE DA - WHILE
+    INDICA O TERMINO DO LAÇO DE DUAS MANEIRAS
+    POR UM NÚMERO QUE INDICA SEU FINAL OU SUA VERIFICAÇÃO POR COMPLETO'''
+
+for numero in range(1, int(input('Digite um número final: ')), 1):
+
+    print(' ' + str(numero))
+
+# range(numero inicial, numero final, quantidade somada ao inicio para final)
