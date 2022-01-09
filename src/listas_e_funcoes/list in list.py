237b3f77f68e08ddd2fa463f8cb6914129c3ceeb
Miguel Tenório <69335370+MiguelHCJS@users.noreply.github.com>
2022-01-09 15:12:01 -0300

    Atualização

---
+++ src/listas_e_funcoes/list in list.py
@@ -0,0 +1,18 @@
+inventario = []
+resposta = input('Inserir equipamento? ')
+while resposta == 's':
+    equipamento = [
+        input('Equipamento: '),
+        float(input('Valor: ')),
+        int(input('Número de série: ')),
+        input('Departamento: ')
+    ]
+    inventario.append(equipamento)
+    resposta = input('Inserir mais um equipamento? ')
+
+for elemento in inventario:
+    print('Nome........: ', elemento[0])
+    print('Valor.......: ', elemento[1])
+    print('Série.......: ', elemento[2])
+    print('Departamento: ', elemento[3])
+    print('---------------------------')
