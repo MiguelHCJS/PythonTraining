1a940aa9ee6657af7bab8903d61a6cf1f5222fdb
Miguel Tenório <69335370+MiguelHCJS@users.noreply.github.com>
2022-01-15 20:15:13 -0300

    dicionario de dados

---
+++ src/dictionary/dicionario.py
@@ -0,0 +1,8 @@
+usuarios = {
+    "Chaves": ["Chaves silva", "17/01/2021", "Ajudante"],
+    "Quico": ["Quico Antonio", "10/02/2021", "Auxiliar"]
+}
+
+print('\n', usuarios)
+
+print("\nDados:", usuarios.get('Quico'))
